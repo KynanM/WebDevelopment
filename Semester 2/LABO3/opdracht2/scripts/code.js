68ec6e2ee14cd3b0b702423582d0f035d80e0cdf
KynanM <kynan.melsens@student.vives.be>
2025-03-11 15:25:32 +0100

    w

--- Semester 2/LABO3/opdracht2/scripts/code.js
+++ Semester 2/LABO3/opdracht2/scripts/code.js
@@ -1,3 +1,13 @@
-const setup = () => {
-}
-window.addEventListener("load", setup);
+window.addEventListener("load", () => {
+    let sliders = document.querySelectorAll("input");
+    let box = document.getElementById("colorBox");
+
+    function updateColor() {
+        let r = sliders[0].value;
+        let g = sliders[1].value;
+        let b = sliders[2].value;
+        box.style.backgroundColor = `rgb(${r},${g},${b})`;
+    }
+
+    sliders.forEach(slider => slider.addEventListener("input", updateColor));
+});
